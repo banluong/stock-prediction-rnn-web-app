f81b636851200f428ce3bb8e19e7afaf8824b6ae
banpython <banluong@hotmail.com>
2020-04-26 14:23:03 -0400

    code cleanup

--- stock_dash.py
+++ stock_dash.py
@@ -38,7 +38,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     html.H3('Enter a stock symbol:', style={'paddingRight': '30px', 'color': colors['text']}),
     dcc.Dropdown(
         id='dropdown_symbol',
-        options = options,
+        options=options,
         value='TSLA',
         multi=False
         # style={'fontSize': 24, 'width': 75}
@@ -47,15 +47,15 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     html.Div([html.H3('Enter start / end date:'),
               dcc.DatePickerRange(
                   id='date_range',
-                  start_date_placeholder_text='Start Date',
+                  #start_date_placeholder_text='Start Date',
                   min_date_allowed=datetime(2015, 1, 1),
                   max_date_allowed=datetime.now(),
                   start_date=datetime(2019, 1, 1),
                   end_date=datetime.now(),
                   number_of_months_shown=2
-                  )
+    )
 
-              ], style={'display': 'inline-block', 'color': colors['text']}),
+    ], style={'display': 'inline-block', 'color': colors['text']}),
     # Submit Button
     html.Div([
         html.Button(id='submit_button',
@@ -73,22 +73,22 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 ])
 
 # app callback to update graph
+
+
 @app.callback(
     Output('graph_candle', 'figure'),
     [Input('dropdown_symbol', 'value'),
      Input('date_range', 'start_date'),
      Input('date_range', 'end_date'),
      Input('submit_button', 'n_clicks')])
-
-def update_graph(symbol,start_date, end_date, n_clicks):
-    if n_clicks==0:
+def update_graph(symbol, start_date, end_date, n_clicks):
+    if n_clicks == 0:
         ticker_data = yf.Ticker('TSLA')
-        df = ticker_data.history(period='1d', start=datetime(2015,1,1), end=datetime.now())
+        df = ticker_data.history(period='1d', start=datetime(2015, 1, 1), end=datetime.now())
 
     else:
         ticker_data = yf.Ticker(symbol)
         df = ticker_data.history(period='1d', start=start_date, end=end_date)
-
 
     first = go.Candlestick(x=df.index,
                            open=df['Open'],
@@ -113,6 +113,7 @@ def update_graph(symbol,start_date, end_date, n_clicks):
 
     figure = {'data': data, 'layout': layout}
     return figure
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
